5b58b4cf19f6d200de2f5a125025fb5a3f6623a3
Hugo Silva <jhssilva96@gmail.com>
2023-05-30 17:03:20 +0200

    Refractor. Add punch in and punch out configuration

--- camera.py
+++ camera.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import cv2 as cv
 import numpy as np
 import os
+import datetime
+import time
+import threading
 
 import tensorflow as tf
 from tensorflow import keras
@@ -18,15 +21,26 @@ GREEN = (0, 255, 0)
 
 unknown_name = "unknown"
 path_train_images_folder = "datasets/train_images"
+path_to_employees_info = "employees_info"
+
+time_between_punch_in_punch_out = 20  # In seconds
 
 
 def camera():
+    # * Define variables employee
+
     global employees_list
     employees_list = get_employees_names_from_file_structure()
 
     if len(employees_list) == 0:
         print("No employees found")
         exit(False)
+
+    global employees_status_has_punched_in
+    employees_status_has_punched_in = set()
+
+    global employee_status_is_blocked
+    employee_status_is_blocked = set()
 
     # * Load model to face classification
     model_name = 'face_classifier_ResNet50Custom.h5'
@@ -78,7 +92,9 @@ def camera():
             # Get the confidence score
             confidence = prediction[0][index]
 
-            color = get_color(index)
+            employee_name = get_employee_name(int(index))
+            handle_punch_in_punch_out(employee_name)
+            color = get_color(employee_name)
 
             cv.rectangle(frame,
                          (x, y),  # start_point
@@ -88,7 +104,7 @@ def camera():
 
             cv.putText(frame,
                        # text to put
-                       "{:6} - {:.2f}%".format(get_employee_name(index).upper(),
+                       "{:6} - {:.2f}%".format(employee_name.upper(),
                                                confidence*100),
                        (x, y),
                        cv.FONT_HERSHEY_PLAIN,  # font
@@ -109,19 +125,19 @@ def camera():
     exit(True)
 
 
-def get_employee_name(index):
+def get_employee_name(index: int) -> str:
     if (index > get_employees_number_with_unknown()):
         return unknown_name
     return employees_list[index]
 
 
-def get_employees_number_with_unknown():
+def get_employees_number_with_unknown() -> int:
     return len(employees_list)
 
 
-def get_color(index):
+def get_color(employee_name: str) -> tuple:
     color = GREEN
-    if (index > get_employees_number_with_unknown() or get_employee_name(index) == unknown_name):
+    if (employee_name == unknown_name):
         color = RED
     return color
 
@@ -132,3 +148,58 @@ def get_employees_names_from_file_structure():
         os.path.join(path_train_images_folder, item))]
     dir_list_ordered = sorted(dirlist)
     return dir_list_ordered
+
+
+def handle_punch_in_punch_out(employee_name):
+    if (employee_name == unknown_name):
+        return
+
+    if (employee_name in employee_status_is_blocked):
+        return
+
+    if (employee_name in employees_status_has_punched_in):
+        punch_in_or_out(False, employee_name)
+    else:
+        punch_in_or_out(True, employee_name)
+
+
+def get_date_time():
+    return datetime.datetime.now()
+
+
+def punch_in_or_out(punch_in: bool, employee_name: str):
+    create_employee_countdown(employee_name)
+    if punch_in:
+        employees_status_has_punched_in.add(employee_name)
+    else:
+        employees_status_has_punched_in.remove(employee_name)
+    message_in_or_out = "in" if punch_in else "out"
+    message = f"{employee_name.capitalize()} punched {message_in_or_out} at {get_date_time()}"
+    write_to_file(employee_name, message)
+    print(message)
+
+
+def write_to_file(employee_name, message):
+    file_name = f"{employee_name}.txt"
+    file_path = os.path.join(path_to_employees_info, file_name)
+
+    with open(file_path, "a") as file:
+        file.write(message + "\n")
+
+
+def create_employee_countdown(employee_name):
+    thread = threading.Thread(target=countdown, args=(
+        time_between_punch_in_punch_out, employee_name))
+    thread.start()
+
+
+def countdown(t, employee_name):
+    employee_status_is_blocked.add(employee_name)
+    while t:
+        time.sleep(1)
+        t -= 1
+
+    employee_status_is_blocked.remove(employee_name)
+
+
+camera()
